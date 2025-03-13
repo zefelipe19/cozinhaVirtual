d93ceb7fa9177a0689735f24125dc00216482498
Felipe <felipe.paula1903@gmail.com>
2025-03-13 16:06:09 -0300

    Adicionando a dependencia do JPA ao pom.xml, criando a entidade do alimento

--- src/main/java/dev/zefelipe19/cozinhaVirtual/CozinhaController.java
+++ src/main/java/dev/zefelipe19/cozinhaVirtual/CozinhaController.java
@@ -1,4 +1,21 @@
 package dev.zefelipe19.cozinhaVirtual;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class CozinhaController {
+    @GetMapping("/")
+    public String helloWorld() {
+        return "<h1>Cozinha Virtual</h1>" +
+                "<p>Esse projeto tem como objetivo te ajudar na cozinha com receitas saborosas.</p>" +
+                "<p>projeto feito com Spring Boot e Java</p>";
+    }
+    @GetMapping("/api/")
+    public String api() {
+
+        return "Comida: " + "quantidade: " ;
+    }
 }
--- src/main/java/dev/zefelipe19/cozinhaVirtual/CozinhaModel.java
+++ src/main/java/dev/zefelipe19/cozinhaVirtual/CozinhaModel.java
@@ -1,4 +1,43 @@
 package dev.zefelipe19.cozinhaVirtual;
 
+import jakarta.persistence.*;
+
+// Definindo uma a classe na tabela do banco de dados.
+@Entity
+@Table(name = "tb_cozinha_virtual")
 public class CozinhaModel {
+
+    @Id // Identificando o atributo id para a tabela no banco de dados.
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Deixando a geração do id sendo sequencial.
+    private Long id;
+    private String name;
+    private int quantity;
+
+    public CozinhaModel() {}
+
+    public CozinhaModel(String name, int quantity, Long id) {
+        this.name = name;
+        this.quantity = quantity;
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
